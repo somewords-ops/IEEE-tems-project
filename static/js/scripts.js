0113d923a211e53ea456a15924c7860c992115d9
somewords-ops <shrinivasvijay48@gmail.com>
2025-02-09 17:26:37 +0530

    Festivals in order

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -53,28 +53,29 @@ if (feedbackForm) {
 
 // Display Festivals on Calendar
 const festivalsData = [
-    { name: 'Diwali', date: '2025-11-01', image: 'https://static.toiimg.com/thumb/msid-114654950,width-400,resizemode-4/114654950.jpg' },
-    { name: 'Holi', date: '2025-03-25', image: 'https://c.files.bbci.co.uk/7C71/production/_128875813_holiepa.jpg' },
-    { name: 'Eid', date: '2025-04-10', image: 'https://cf-img-a-in.tosshub.com/sites/visualstory/wp/2024/04/PTI06_29_2_1712735287-2-scaled.jpg?size=*:900' },
-    { name: 'Durga Puja', date: '2025-10-11', image: 'https://images.hindustantimes.com/img/2022/09/30/1600x900/durga_puja_1664502025274_1664502025551_1664502025551.jpg' },
-    { name: 'Navratri', date: '2025-09-28', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI1GJLGZr-YinruQc3WkxsiWN1_GMR3wKv6w&s' },
+    { name: 'Lohri', date: '2025-01-13', image: 'https://www.hindustantimes.com/ht-img/img/2024/01/10/1600x900/Delhi-residents-are-set-to-celebrate-Lohri-dancing_1673612444097_1704878438455.jpg' },
     { name: 'Pongal', date: '2025-01-14', image: 'https://static.toiimg.com/thumb/msid-73202857,width-400,resizemode-4/73202857.jpg' },
-    { name: 'Onam', date: '2025-08-21', image: 'https://bsmedia.business-standard.com/_media/bs/img/misc/2015-08/28/full/1440742999-27081390.jpg?im=FeatureCrop,size=(826,465)' },
-    { name: 'Christmas', date: '2025-12-25', image: 'https://hips.hearstapps.com/hmg-prod/images/decorated-christmas-tree-near-fireplace-at-home-royalty-free-image-1731335629.jpg?crop=1xw:0.84375xh;center,top&resize=1200:*' },
     { name: 'Makar Sankranti', date: '2025-01-14', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiKSBzpvhyw819HmME1v21ThDHWXDh5cjp5w&s' },
+    { name: 'Republic Day', date: '2025-01-26', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8owsyPn62-Px6zCU-TuGg-rpX6ZJTczAFIQ&s' },
+    { name: 'Mewari New Year', date: '2025-03-23', image: 'https://images.moneycontrol.com/static-mcnews/2025/01/20250101025320_5-am.png?impolicy=website&width=770&height=431' },
+    { name: 'Holi', date: '2025-03-25', image: 'https://c.files.bbci.co.uk/7C71/production/_128875813_holiepa.jpg' },
+    { name: 'Ugadi', date: '2025-04-04', image: 'https://mangalfashions.com/cdn/shop/articles/How-to-celebrate-Ugadi-the-traditional-way-Mangal-Fashions-Indian-Home-Decor-and-Craft-456.jpg?v=1681732022' },
+    { name: 'Eid', date: '2025-04-10', image: 'https://cf-img-a-in.tosshub.com/sites/visualstory/wp/2024/04/PTI06_29_2_1712735287-2-scaled.jpg?size=*:900' },
+    { name: 'Baisakhi', date: '2025-04-13', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEjFJFMPAPn0dYyZB4wQtcRNXSlsKeX7hd_Q&s' },
+    { name: 'Buddha Purnima', date: '2025-05-23', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnBnUsF70qt_mLVhKwRUtyRazx2eEzPOyd2A&s' },
+    { name: 'Independence Day', date: '2025-08-15', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/A_still_of_Red_Fort%2C_during_the_62nd_Independence_Day_celebrations%2C_in_Delhi_on_August_15%2C_2008.jpg/800px-A_still_of_Red_Fort%2C_during_the_62nd_Independence_Day_celebrations%2C_in_Delhi_on_August_15%2C_2008.jpg' },
     { name: 'Raksha Bandhan', date: '2025-08-19', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiKSBzpvhyw819HmME1v21ThDHWXDh5cjp5w&s' },
+    { name: 'Onam', date: '2025-08-21', image: 'https://bsmedia.business-standard.com/_media/bs/img/misc/2015-08/28/full/1440742999-27081390.jpg?im=FeatureCrop,size=(826,465)' },
     { name: 'Janmashtami', date: '2025-08-29', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQynmdQcA5fI3GQeRmb_mbMxPMi8HnqXzYAOA&s' },
     { name: 'Ganesha Chaturthi', date: '2025-09-07', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Lalbaugh_Ganesha.jpg/1200px-Lalbaugh_Ganesha.jpg' },
-    { name: 'Baisakhi', date: '2025-04-13', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEjFJFMPAPn0dYyZB4wQtcRNXSlsKeX7hd_Q&s' },
-    { name: 'Karva Chauth', date: '2025-10-19', image: 'https://cdn0.weddingwire.in/article/4644/3_2/1280/jpg/124464-karwa-chauth-2024.webp' },
-    { name: 'Lohri', date: '2025-01-13', image: 'https://www.hindustantimes.com/ht-img/img/2024/01/10/1600x900/Delhi-residents-are-set-to-celebrate-Lohri-dancing_1673612444097_1704878438455.jpg' },
-    { name: 'Buddha Purnima', date: '2025-05-23', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnBnUsF70qt_mLVhKwRUtyRazx2eEzPOyd2A&s' },
     { name: 'Mahalaya', date: '2025-09-15', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcFcA9VD7JChbbOOEK1ZaEFPDCsyrQIIqdpA&s' },
-    { name: 'Independence Day', date: '2025-08-15', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/A_still_of_Red_Fort%2C_during_the_62nd_Independence_Day_celebrations%2C_in_Delhi_on_August_15%2C_2008.jpg/800px-A_still_of_Red_Fort%2C_during_the_62nd_Independence_Day_celebrations%2C_in_Delhi_on_August_15%2C_2008.jpg' },
-    { name: 'Republic Day', date: '2025-01-26', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8owsyPn62-Px6zCU-TuGg-rpX6ZJTczAFIQ&s' },
-    { name: 'Ugadi', date: '2025-04-04', image: 'https://mangalfashions.com/cdn/shop/articles/How-to-celebrate-Ugadi-the-traditional-way-Mangal-Fashions-Indian-Home-Decor-and-Craft-456.jpg?v=1681732022' },
-    { name: 'Mewari New Year', date: '2025-03-23', image: 'https://images.moneycontrol.com/static-mcnews/2025/01/20250101025320_5-am.png?impolicy=website&width=770&height=431' }
+    { name: 'Navratri', date: '2025-09-28', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI1GJLGZr-YinruQc3WkxsiWN1_GMR3wKv6w&s' },
+    { name: 'Durga Puja', date: '2025-10-11', image: 'https://images.hindustantimes.com/img/2022/09/30/1600x900/durga_puja_1664502025274_1664502025551_1664502025551.jpg' },
+    { name: 'Karva Chauth', date: '2025-10-19', image: 'https://cdn0.weddingwire.in/article/4644/3_2/1280/jpg/124464-karwa-chauth-2024.webp' },
+    { name: 'Diwali', date: '2025-11-01', image: 'https://static.toiimg.com/thumb/msid-114654950,width-400,resizemode-4/114654950.jpg' },
+    { name: 'Christmas', date: '2025-12-25', image: 'https://hips.hearstapps.com/hmg-prod/images/decorated-christmas-tree-near-fireplace-at-home-royalty-free-image-1731335629.jpg?crop=1xw:0.84375xh;center,top&resize=1200:*' }
 ];
+
 
 
 const calendarContainer = document.querySelector('#festival-calendar');
